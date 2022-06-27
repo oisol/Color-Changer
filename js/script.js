cb598b29827ca41a4ccc8fa2fa7aabc9ce21f275
Matheus <matheusroca11@gmai.com>
2022-06-27 18:11:04 -0300

    Cópia rápida com clique

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function rgbToHex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
 
-function changeColor (event) {
+function changeColor () {
     n1 = Math.floor((Math.random() * 255) + 1)
     n2 = Math.floor((Math.random() * 255) + 1)
     n3 = Math.floor((Math.random() * 255) + 1)
@@ -21,5 +21,27 @@ function changeColor (event) {
     hex.innerText = `${rgbToHex(n1, n2, n3)}`; 
 }
 
+function copyRGB () {
+  navigator.clipboard.writeText(rgb.innerText);
+  swal({
+    title: "Cor em RGB copiada!",
+    text: "",
+    icon: "success",
+    button: "Yayyy",
+  });
+}
+
+function copyHex () {
+  navigator.clipboard.writeText(hex.innerText);
+  swal({
+    title: "Cor em Hexadecimal copiada!",
+    text: "",
+    icon: "success",
+    button: "o(≧▽≦)o",
+  });
+}
+
 botao.addEventListener("click", changeColor)
+rgb.addEventListener("click", copyRGB)
+hex.addEventListener("click", copyHex)
 
